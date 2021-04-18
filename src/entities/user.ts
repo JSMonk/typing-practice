ce923c273caa2c810d30e7cf03ea39c002ed3ac6
Vladislav Boldakov <boldakov.vladislav@gmail.com>
2021-04-18 19:55:57 +0700

    feat: use or for guard

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,11 +1,18 @@
 import { Admin } from "./admin";
 import type { Client } from "./client";
 import { Moderator } from "./moderator";
+import or from "../utils/or";
 
 export type User = Admin | Client | Moderator;
-export type LoggedUser = Admin | Client |  Moderator;
+export type LoggedUser = Admin | Client | Moderator;
 export type PrivilegedUser = Admin | Moderator;
 
 export function isPrivilegedUser(user: LoggedUser): user is PrivilegedUser {
-    return Admin.is(user) || Moderator.is(user);
+  try {
+    or(Moderator, Admin);
+    return true;
+  } catch (e) {
+    alert(e);
+    return false;
+  }
 }
