48991ab49ed1e8dfdf28fb421d1a5b854397f335
vladislav boldakov <boldakov.vladislav@gmail.com>
2021-04-24 13:52:42 +0700

    fix: error

--- src/hooks/use-operations.ts
+++ src/hooks/use-operations.ts
@@ -2,7 +2,7 @@ import Services from "../services";
 import { useContext } from "react";
 import type { User } from "../entities/user";
 
-export default function useOperations<U1 extends User>(user: U1, currentUser: U1) {
+export default function useOperations<U1 extends User, U2 extends User>(user: U1, currentUser: U2) {
   const { userService } = useContext(Services);
   return userService.getAvailableOperations(user, currentUser);
 }
